3d9c2d275ea6fbfe7425ba167366962d5fe7578c
Hal <u58693@student.wat.edu.pl>
2025-12-15 21:09:44 +0100

    2 test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -12,14 +12,4 @@ public void compare(){
     }
 }
 
-    @Test
-    public void compare2(){
-        int a=10;
-        int b=20;
-
-        if(a!=b){
-            System.out.println("a!=b");
-        }
-    }
-
 }
