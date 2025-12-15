31f54eb437377966f17498b973b6a1ea6993100b
Hal <u58693@student.wat.edu.pl>
2025-12-15 20:45:39 +0100

    Initial commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class SampleTest {
+
+@Test
+public void compare(){
+    int a=10;
+    int b=20;
+
+    if(a==b){
+        System.out.println("a==b");
+    }
+}
+
+}
