b0d63abe8776c5d6b98345fecfa5f8e6b683606e
ChalimoniukKX3 <u58693@student.wat.edu.pl>
2026-01-11 13:29:33 +0100

    Create CI_testing.java

---
+++ src/test/java/pl/demo/CI_testing.java
@@ -0,0 +1,17 @@
+package pl.demo;
+
+public class CI_testing {
+
+    public static int divide(int a, int b) {
+        return a / b; // !! bug: potencjalne dzielenie przez 0 !!
+    }
+
+    public static String getPassword() {
+        return "admin123"; // !! code smell: hardcoded secret !!
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divide(10, 0)); // !! 100% crash !!
+    }
+}
+//TEST
