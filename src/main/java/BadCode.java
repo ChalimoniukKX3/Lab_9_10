eb7f62f9fdbbcf6fccc262f3a846e3a9634e15a8
ChalimoniukKX3 <u58693@student.wat.edu.pl>
2026-01-11 21:41:54 +0100

    Create BadCode.java

---
+++ src/main/java/BadCode.java
@@ -0,0 +1,14 @@
+public class BadCode {
+
+    public static int divide(int a, int b) {
+        return a / b; // !! bug: potencjalne dzielenie przez 0 !!
+    }
+
+    public static String getPassword() {
+        return "admin123"; // !! code smell: hardcoded secret !!
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divide(10, 0)); // !! 100% crash !!
+    }
+}
